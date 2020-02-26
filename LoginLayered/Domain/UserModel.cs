f8c821b3ab4c1171273b9df9c0615024e54ccb89
jhasmany_jhunnior@outlook.com <jhasmany>
2020-02-25 22:51:17 -0400

    actualizando repositorio

---
+++ LoginLayered/Domain/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Domain
+{
+    public class UserModel
+    {
+        UserDao userDao = new UserDao();
+        public bool LoginUser(string user, string pass) {
+            return userDao.Login(user, pass);
+        }
+    }
+}
